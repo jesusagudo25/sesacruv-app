ae203dfadef218cf3ee75ca03e6ccf0f0ace260a
Jesus Agudo <jagudo2514@gmail.com>
2022-07-26 01:20:11 -0500

    second commit

--- src/screens/Information/Requirements.js
+++ src/screens/Information/Requirements.js
@@ -1,11 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, Button,StyleSheet } from "react-native";
+import { Video, AVPlaybackStatus } from 'expo-av';
+import React, {Component} from 'react';
 
 const Requirements = () => {
+    const video = React.useRef(null);
+    const [status, setStatus] = React.useState({});
     return (
-        <View>
-            <Text>Requirements</Text>
-        </View>
+      <View style={styles.container}>
+        <Video
+          ref={video}
+          style={styles.video}
+          source={{
+            uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+          }}
+          useNativeControls
+          resizeMode="contain"
+          isLooping
+          onPlaybackStatusUpdate={status => setStatus(() => status)}
+        />
+      </View>
     );
 }
-
 export default Requirements;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',
+    },
+    video: {
+      alignSelf: 'center',
+      width: 320,
+      height: 200,
+    },
+    buttons: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
